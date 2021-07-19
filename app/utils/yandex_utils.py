96f6139e61d32ff462424b505f91e5f55478e428
ducbv <ducbvbk@gmail.com>
2021-07-19 22:40:58 +0700

    hide yarndex key

--- app/utils/yandex_utils.py
+++ app/utils/yandex_utils.py
@@ -1,11 +1,7 @@
-from decimal import Decimal
-
 from yandex_geocoder import Client
 
+client = Client("")
 
-client = Client("af81ec10-d933-43fb-90f0-6157977e812d")
-
-
-coordinates = client.coordinates("Москва Льва Толстого 16")
+coordinates = client.coordinates("MKAD")
 
 print(coordinates)
